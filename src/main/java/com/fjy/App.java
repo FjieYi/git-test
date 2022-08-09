987d90ae60d0316207a93b0ae5a08fbe2c3f3f14
FjieYi <1197562890@qq.com>
2022-08-09 11:46:13 +0800

    learn rabase2

--- src/main/java/com/fjy/App.java
+++ src/main/java/com/fjy/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("220809-1");
+        System.out.println("220809-2");
         System.out.println("add code 5");
         System.out.println( "Hello World!" );
     }
