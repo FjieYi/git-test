3012442d12743f4bf97d31979ec7d1597d1825ae
zhux <zhuxin@weibopay.com>
2022-08-09 11:06:27 +0800

    learn rabase1

--- src/main/java/com/fjy/App.java
+++ src/main/java/com/fjy/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("220809-1");
         System.out.println("add code 5");
         System.out.println( "Hello World!" );
     }
